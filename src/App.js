d752ad1b520b39e1a5eb83e01ce0fea2cd2a08b9
Shaja Nasir <Shajanasir@gmail.com>
2023-06-21 10:26:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Shaja Nasir </h1>
+      <h2>Intervest Software Technologies</h2>
+    </div>
+  );
+}
